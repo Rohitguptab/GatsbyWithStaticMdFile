67ed5951cea0b0521656d7ea5e04cff382f4322b
Rohit Gupta <rohit.gupta@multidots.com>
2019-02-15 12:09:50 +0530

    added all files

---
+++ gatsby-config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  siteMetadata: {
+    title: `My Blog - Rohit Gupta`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "pages"
+      }
+    },
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        typeName: "gupta",
+        fieldName: "rohit",
+        url: "https://jsonplaceholder.typicode.com/posts/"
+      }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`
+      }
+    }
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.app/offline
+    // 'gatsby-plugin-offline',
+  ]
+};
---
+++ src/templates/blog-post.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Layout from "../components/layout";
+import Img from "gatsby-image";
+import Metatags from "../components/Metatags";
+import { graphql } from "gatsby";
+function BlogPost(props) {
+  console.log(props);
+  const post = props.data.markdownRemark;
+  const url = props.location.host;
+  const thumbnail = post.frontmatter.image.childImageSharp.resize.src;
+  const { title } = post.frontmatter;
+  const { description } = post.frontmatter;
+  return (
+    <Layout>
+      <Metatags
+        title={title}
+        description={description}
+        thumbnail={url + thumbnail}
+        url={url}
+        pathname={props.location.pathname}
+      />
+      <div>
+        <h1>{title}</h1>
+        <Img fluid={post.frontmatter.image.childImageSharp.fluid} />
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+    </Layout>
+  );
+}
+
+export default BlogPost;
+export const query = graphql`
+  query PostQuery($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        description
+        image {
+          childImageSharp {
+            resize(width: 1500, height: 1500) {
+              src
+            }
+            fluid(maxWidth: 786) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
